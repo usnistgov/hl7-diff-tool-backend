5918b0938380f7d8de198e8499497faa52f9752e
Ismail Mellouli <ismail.mellouli@nist.gov>
2021-09-16 09:31:41 -0400

    fix

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use(function(req, res, next) {
 //   return res.status(500).json(response);
 // });
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8107);
 
 let server = app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + server.address().port+" Environment: ",process.env.ENV);
